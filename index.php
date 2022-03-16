eeb93ba340b38a9278f322d752cd74082f32e793
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-03-16 13:12:00 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en-us">
-  <head>
-  </head>
-  
-  <body>
-  
-  <?php
-  
-  header("HTTP/1.0 404 Not Found");
-
-  
-  ?>
-  
-  
-
-  </body>
+<html>
+ <head>
+  <title>Error 404</title>
+ </head>
+ <body>
+ <?php header( $_ENV['SERVER_PROTOCOL']." 404 Not Found", true ); ?> 
+ </body>
 </html>
