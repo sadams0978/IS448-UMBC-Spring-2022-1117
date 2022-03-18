1f6a20f1179e84aad0d9a5be902828ec54c3e787
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-03-18 18:23:03 -0400

    Update case2.php

--- uc2-favorites/case2.php
+++ uc2-favorites/case2.php
@@ -3,45 +3,65 @@
 	<head>
 		<meta charset = "utf-8">
 		<link rel = "stylesheet" type = "text/css" href = "../root.css"/>
-		<link rel = "stylesheet" type = "text/css" href = "uc2-favorites.css"/>
+		<link rel = "stylesheet" type = "text/css" href = "uctwo"/>
 		<script src="https://kit.fontawesome.com/be0f7619b0.js" crossorigin="anonymous"></script>
 		
 		<title> Favtorites </title>
 	</head>
 	
 	<body>
-
+	
 		<?php
 		// Includes our menu bar, instead of copying and pasting through the pages
 		include('../menu.html');
 		?>
-	
-	<p class="spacer"></p>
-	
-
 		
-		<p class = "zone"></p>
-			<ul>
-				<li> <a href = "">Home </a></li>
-				<li> <a href = "">Go to Favorites / Tracked Cards </a></li>
+		<!-- favorites section -->
+		<div class = "favoriteCardsContainer">
+			<h2> Filters: </h2>
+				<label for = "category"> Category: </label>
+					<select name = "category" id = "category">
+						<option value="" disabled selected>Select your option</option>
+					</select>
 				
+				</br>	
+					
+				<label for = "category"> Other Category </label>
+					<select name = "category" id = "category">
+						<option value="" disabled selected>Select your option</option>
+					</select>
+					
+				</br>	
+
+				<label for = "category"> Other Category </label>
+					<select name = "category" id = "category">
+						<option value="" disabled selected>Select your option</option>
+					</select>	
+		
+		</div>
+		
+		<!-- cards section -->
+		<div class = "cardsContainer">
+			<ul>
+					<li ><i class="fa-solid fa-heart"></i><img src = "" alt = "favorite card"/>
+					<br>Current Price: $ <button type="button" onclick="alert('Card Added to Shopping Card')">Add to Cart</button></li>
+		
 			</ul>
 			
-		<div class = "appfilters">
-			<p class = "filters">Filters </p>
+			<ul>
+					<li ><i class="fa-solid fa-heart"></i><img src = "" alt = "favorite card"/>
+					<br>Price: $ <button type="button" onclick="alert('Card Added to Shopping Card')">Add to Cart</button></li>
+		
+			</ul>
 		</div>
 		
-		<div class = "favoritecards">
-			
-			<p class = "favcarddetail">Image of favorite card: Price<br /></p>
-			
-			<p class = "favcarddetail">Image of second row favorite card: Price<br /></p>
+		</br>
 		
-		</div>
-			
 		<div class = "footer">
-			<p>by dylan de leon</p>
+			Made By: Dylan De Leon
 		</div>
-		
+	
 	</body>
+	
+	
 </html>
