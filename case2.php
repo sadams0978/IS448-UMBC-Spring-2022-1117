21722aa40e61c71af4f12ebf88ce13e0d6c4f09c
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-03-18 00:23:19 -0400

    Add files via upload

---
+++ case2.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<link rel = "stylesheet" type = "text/css" href = "casestyle.css"/>
+		
+		<title> Favtorites </title>
+	</head>
+	
+	<body>
+		<p class = "login">you are logged in as ABC <a href = ""><img src = "" alt = "shoppingcart" /></a></p>
+		
+		<div class = "searchbar">
+			
+			<p><input type = "text" name = "search" /> <button type = "submit">Search </button></p>
+		
+		</div>
+		
+		<p class = "zone"></p>
+			<ul>
+				<li> <a href = "">Home </a></li>
+				<li> <a href = "">Go to Favorites / Tracked Cards </a></li>
+				
+			</ul>
+			
+		<div class = "appfilters">
+			<p class = "filters">Filters </p>
+		</div>
+		
+		<div class = "favoritecards">
+			
+			<p class = "favcarddetail">Image of favorite card: Price<br /></p>
+			
+			<p class = "favcarddetail">Image of second row favorite card: Price<br /></p>
+		
+		</div>
+			
+		<div class = "footer">
+			<p>by dylan de leon</p>
+		</div>
+		
+	</body>
+</html>
