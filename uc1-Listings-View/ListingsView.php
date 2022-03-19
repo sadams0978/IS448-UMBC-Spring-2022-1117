302c9db385789ac863c29fb5f46b8a969413690f
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-03-18 21:53:11 -0400

    Update and rename ListingsView.html to ListingsView.php

--- uc1-Listings-View/ListingsView.php
+++ uc1-Listings-View/ListingsView.php
@@ -12,19 +12,11 @@
 </head>
 <body>
  
-	<p class="login">you are signed in as XYZ <a href="../uc4-Seller-listing/usecase4.php"> <i class="fa-solid fa-cart-shopping"></i> </a> </p>
-	<!--link to shopping cart and sign in pages-->
+	<?php
+	// Includes our menu bar, instead of copying and pasting through the pages
+	include('../menu.html');
+	?>
 	
-	
-	<p class="spacer"></p>
-		<ul class="header">
-			<li><a href="../index.html">Home</a></li>
-			<li><a href="../uc2-favorites/case2.php">Favorite Cards</a></li>
-			<li><a href="../uc3-purchasing/card-details-Andrew-part3.php">Purchasing</a></li>
-			<li><a href="../uc5-login/index.php">Login</a></li>
-  			<div class="search"><li><input type="text" name="Search" /> <button type="submit">Search</button></li></div>
-		</ul>
-
 	
 	
 	<div class ="cardDetailsContainer"> 
