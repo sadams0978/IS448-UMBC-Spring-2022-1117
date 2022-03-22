e7cedc3bbb272fdf1c5577a773ce26aad1c5c833
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-03-22 11:47:21 -0400

    Update login.php

--- uc5-login/login.php
+++ uc5-login/login.php
@@ -8,22 +8,21 @@
  $passwordinvalid = "This e-mail entry is invalid, please try again. ";
  $email = $_POST['email'];
  $password = $_POST['password'];
- $homepage = "https://swe.umbc.edu/~samuela3/login";
-
-
+ $login = "index.php";
+ $homepage = "../uc1-Listings-View/ListingsView.php";
  //Checking to see if the e-mail or password is empty and exiting
     if(empty ($email || $password)){
-        header('Location: '.$homepage);
+        header('Location: '. $login);
         die;   
     }
 
 
+  //Starts up a session with the E-Mail address and redirects back to homepage
   session_start ();
-  
   $_SESSION['email'] = ('Welcome, ' . $email); 
+  header('Location: '.$homepage);
+        die;   
   
-  
-  echo $_SESSION['email'];
  ?>
 
 
