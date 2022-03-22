e85cc4b8756e4f02185762608c8808c2a453bb71
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-03-22 13:52:31 -0400

    Create logout.php

---
+++ uc5-login/logout.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>
+    Login
+  </title>
+  <link rel="stylesheet" href="style.css" >
+  <meta charset="UTF-8">
+  <script src="https://kit.fontawesome.com/be0f7619b0.js" crossorigin="anonymous"></script>
+	
+</head>
+
+<body>
+
+  <?php
+	session_destroy();
+	?>
+	
+  
+  
+    <div class="Login_Box">
+
+      <h1> You have been logged out successfully!</h1> 
+      <h3> <strong> <a href="../index.html" class = "Site_Link"> Click here to go back</a> </strong> </h3>
+
+      
+      <h3> <a href="create.php" class = "Site_Link"> Create An Account</a> </h3>
+      <h3> <a href="change_password.php" class = "Site_Link"> Change Your Password</a> </h3>
+      <h3> Need Help? </h3>
+      <h3> Contact IT at x1033 or by e-mailing samuela3@umbc.edu</h3>
+      <h3> <strong> Unauthorized Access is Prohibited </strong> </h3>
+      <h3> <strong> <a href="https://www.umbc.edu/policies/pdfs/UMBC%20X1.00.06%20UMBC%20Policy%20on%20Web%20Site%20Privacy%20Statement.pdf#:~:text=It%20is%20university%20policy%20that,sites%20not%20affiliated%20with%20UMBC." class = "Site_Link"> Privacy Policy</a> </strong> </h3>
+
+
+             <i class="fa-brands fa-instagram fa-xl"></i>
+             <i class="fa-brands fa-facebook fa-xl"></i>
+             <i class="fa-brands fa-twitter fa-xl"></i>
+
+</div>
+  
+</body>
+
+</html>
+
