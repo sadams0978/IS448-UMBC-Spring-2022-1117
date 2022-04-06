f620f3fff6f036e03cb6e04ce45c75bf53e2f34d
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-06 19:09:22 -0400

    Update password_update.php

--- uc5-login/password_update.php
+++ uc5-login/password_update.php
@@ -41,6 +41,7 @@
 
     if(!$min_length || !$contains_uppercase || !$contains_digit || !$contains_special_char) {
     echo 'Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.';
+     die;
     }
 
 
