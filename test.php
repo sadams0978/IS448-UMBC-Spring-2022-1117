fcdfa0c5aae40a90d0e8c4094fbe710e148cf222
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-07 11:56:32 -0400

    Update test.php

--- test.php
+++ test.php
@@ -5,9 +5,18 @@
   
   // The hash of the password that
   // can be stored in the database
-  $hash = password_hash($plaintext_password, 
-          PASSWORD_DEFAULT);
-  
+  $hash = ("$2y$10$YBfhBfC.F7bK7pMA1QX0EOUM9zBmnNv2KGFAEn1T95oxHNtye0bbW");
   // Print the generated hash
-  echo "Generated hash: ".$hash;
+
+
+
+  // Verify the hash against the password entered
+  $verify = password_verify($plaintext_password, $hash);
+  
+  // Print the result depending if they match
+  if ($verify) {
+      echo 'Password Verified!';
+  } else {
+      echo 'Incorrect Password!';
+  }
 ?>
