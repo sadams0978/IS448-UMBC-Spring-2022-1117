ff1b5cbba294e61cf0bbe8e4a5c3d6c63f9183da
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-07 11:45:08 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,12 @@
 <?php
-$password_hash = (password_hash("password", PASSWORD_DEFAULT);
-var_dump($password_hash);
+   $passw01 = "password";
+
+
+
+$hashp04 = password_hash($passw01, PASSWORD_BCRYPT;
+echo strlen($hashp04) . " characters
+" . $hashp04;
+
 
 
 
