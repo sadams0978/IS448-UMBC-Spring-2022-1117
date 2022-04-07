ac894204eaf83f5a2599428e1935778527a1552d
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-07 11:34:01 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,14 +1,5 @@
 <?php
-$servername = "192.168.254.2";
-$username = "IS448";
-$password = "IS448password";
+echo (password_hash('password', PASSWORD_DEFAULT);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
 ?>
