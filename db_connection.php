f299e78e0ce832a0b85c21e5304dde429510835f
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-08 10:31:12 -0400

    Create db_connection.php

---
+++ db_connection.php
@@ -0,0 +1,3 @@
+  //Connect to DB this will be utilized in several sites
+    $db = mysqli_connect ("192.168.254.2", "IS448", "IS448password", "samuela3");
+    if (mysqli_connect_errno())	exit("Error - could not connect to MySQL");
