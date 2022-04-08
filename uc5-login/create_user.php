1e4184e7ba9ecef8bad5d49b4a9eaad3c0473083
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-08 11:00:26 -0400

    Update create_user.php

--- uc5-login/create_user.php
+++ uc5-login/create_user.php
@@ -48,9 +48,9 @@
      die;
     }
   
-  
-	// Includes our Database Connection Details
-	include('../db_connection.php');
+		// Includes our Database Connection Details
+    	$db = mysqli_connect ("192.168.254.2", "IS448", "IS448password", "samuela3");
+   	if (mysqli_connect_errno())	exit("Error - could not connect to MySQL");
 
      //Hashing our new password
       $hash = password_hash($password, PASSWORD_DEFAULT);
