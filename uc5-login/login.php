ff8913a4ee62b267e9cd2551012b3867f7733e0d
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-08 10:33:02 -0400

    Update login.php

--- uc5-login/login.php
+++ uc5-login/login.php
@@ -14,11 +14,9 @@
     }
 
     
-//Connect to DB
-    $db = mysqli_connect ("192.168.254.2", "IS448", "IS448password", "samuela3");
-       
-       if (mysqli_connect_errno())	exit("Error - could not connect to MySQL");
-       
+		// Includes our Database Connection Details
+		include('../db_connection.php');
+
 //Selecting the email_address and password from DB
      $select = "select email_address, password from login where email_address = '$email'";  
      $result = mysqli_query($db, $select);
