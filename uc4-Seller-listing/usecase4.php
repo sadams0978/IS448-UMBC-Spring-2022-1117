88fb6f10322a313fce26768e58971e61fe69c0e9
RedFalcon9 <99211482+RedFalcon9@users.noreply.github.com>
2022-04-13 18:42:09 -0400

    fixing form attempt 1.1.1
    everything from 1.1 and earlier was not updating from my local files to the repo. Trying this way.

--- uc4-Seller-listing/usecase4.php
+++ uc4-Seller-listing/usecase4.php
@@ -3,10 +3,6 @@
 <head>
 <meta charset="utf-8">
 
-<!--
-<link rel="stylesheet" type="text/css" href="root.css"/>
-<link rel="stylesheet" type="text/css" href="Andrew-part3.css"/>
---> 
 
 <link rel="stylesheet" type="text/css" href="root.css"/>
 <link rel="stylesheet" type="text/css" href="usecase4.css"/>
@@ -17,123 +13,107 @@
 <!--get name of card from sql -->
 </head>
 <body>
-
-	
-	
-		<?php
+	<?php
 		// Includes our menu bar, instead of copying and pasting through the pages
-		include('../menu.php');
-		?>
-	
-<!-- seller enters picture for card listing--> 
-<div class="left">
-	<p class="card"><img src="../uc2-favorites/blank-card.jpg" alt="Playing Card image"  width = "300" height = "400"/></p>
-	<form action="">
-  		<input type="file" id="myFile" name="filename">
-	</form>
-</div>	
+	include('../menu.php');
+	?>
 
-<div class="right">
-	
+	<?php
+		//assign input in form to php variables
+		/* card image, implement later
 
-	<!-- Upper box containing text boxes and dropdowns for card infomrmation--> 
-	<div class ="cardDetailsContainer"> 
+		$cardimage = $_POST["cardimage"]; 
 
-		<!--Enter card category --> 
-		<label for="category"> Category:</label> <!--from sql & php-->
-			<select name= "category" id="category"> 
-				<option value="" disabled selected>Select your option</option> <!-- acts as place holder prior to user interacting with element--> 
-				<option value=”1”>Pokémon</option>
-				<option value=”2”>Yu Gi Oh</option>
-				<option value=”3”>Baseball</option>
-				<option value=”4”>Football</option>
-				<option value=”4”>Other</option>
-			</select>
-		
-		<!--Enter card condition--> 
-		<label for="condition"> Condition:</label> <!--from sql & php-->
-			<select name= "condition" id="condition"> 
-				<option value="" disabled selected>Select your option</option>
-				<option value=”1”>Poor</option>
-				<option value=”2”>Average</option>
-				<option value=”3”>Great</option>
-				<option value=”4”>Mint</option>
-			</select>
+		*/
 
-		<br/> 
-		
+		$category = $_POST["category"]; 
+		$condition = $_POST[:condition];
+		$finish = $_POST["finish"];
+		$composition = $_POST["composition"];
+		$year = $_POST["year"];
+		$description = $_POST["description"];
+		$sellerprice = $_POST["sellerprice"];
+		$quantity = $_POST["quantity"];
 
-		<!--Enter card finish--> 
-		<label for="finish"> Finish:</label> <!--from sql & php-->
-			<select name= "finish" id="finish"> 
-				<option value="" disabled selected>Select your option</option>
-				<option value=”1”>Matte</option>
-				<option value=”2”>Satin</option>
-				<option value=”3”>Gloss</option>
-				<option value=”4”>Other</option>
-			</select>
+		/* idea: iterate through $post array using foreach loop. 
+		foreach ($_POST as $field => $cardattributevalue)
+		{
+			(isset($_POST["category"]) && (!empty($_POST["category"]))) 
 
-		<!-- Enter card composition--> 
-			<label for="composition"> Composition:</label> <!--from sql & php-->
-			<select name= "composition" id="composition"> 
-				<option value="" disabled selected>Select your option</option>
-				<option value=”1”>paperboard</option>
-				<option value=”2”>thick paper</option>
-				<option value=”3”>plastic</option>
-				<option value=”4”>metal</option>
-				<option value=”5”>other</option>
+		}
 
-			</select>
+		*/
 
-		<!--Enter card year--> 
-		<form action=" "> <!--php stuff--> 
-			<label for ="year"> Year </label>
-			<input type="text" name="year" id= "year" size ="4" placeholder="Year" minlength="4" maxlength ="4" > <!--ensures that a year in four digit form e.g. 1999 is entered--> 
-		</form>	
 
-	</div>
-	
 
-	<!-- lower box containing a big text box for user to provide more specific/writtn outdetails about the card --> 
 
-	<div class ="cardDetailsContainer"> 
-		<form action=" "> <!-- php stuff --> 
-			<textarea id="description" rows="15" cols = "79" required>
-			</textarea>
-		</form>
+		if(
+			(isset($_POST["category"]) && (!empty($_POST["category"]))) &&
+			(isset($_POST["condition"]) && (!empty($_POST["condition"]))) &&
+			(isset($_POST["finish"]) && (!empty($_POST["finish"]))) &&
+			(isset($_POST["composition"]) && (!empty($_POST["composition"])) &&
+			(isset($_POST["year"]) && (!empty($_POST["year"])) &&
+			(isset($_POST["composition"]) && (!empty($_POST["composition"])) &&
+			(isset($_POST["year"]) && (!empty($_POST["year"])) &&
+			(isset($_POST["description"]) && (!empty($_POST["description"])) &&
+			(isset($_POST["sellerprice"]) && (!empty($_POST["sellerprice"])) &&
+			(isset($_POST["quantity"]) && (!empty($_POST["quantity"])) /* &&
+
+			check for image. implement later
+
+			(isset($_POST["cardimage"]) && (!empty($_POST["cardimage"])) */
+		)
+		{
+
+
+		//insert into database with sql 
+
+
+		//Thank user message and offer to return to shopping or make another listing
+	?> 
+
+	<div class = "cardDetailsContainer"> 
+		<p class = "center">
+			Thank you! Your listing has been posted to our site. </br> 
+			Click  <a href="https://jenkins-build.arlcyber.me/uc4-Seller-listing/seller_listing.html">here</a> to post another listing. </br> 
+			OR go <a href="https://jenkins-build.arlcyber.me/uc1-Listings-View/ListingsView.php">back</a> to continue shopping. 
+		</p>
 	</div>
 
+	<?php
+
+	
 
 
-		
-	<div class="pricing">
-		<!--pricing information-->
-		<form action =""> 
-			<label for ="sellerprice"> Price per unit:</label>
-			<input type="text" id="sellerprice" class ="priceandquantity" size ="7">
-		</form>
 
-		<!--quantity information--> 
-		<form action =""> 
-			<label for ="sellerquantity"> Quantity:</label>
-			<input type="text" id="sellerquantity" class ="priceandquantity" size ="7" maxlength ="4">
-		</form>
 
-		<!-- button for seller to post listing --> 
-		<p><button type="submit">Post listing</button><!--input into cart table to sql--></p>
+		}else{
 
+		//message to user to go back and entery information in all fields 
+	?>
+
+	<div class = "cardDetailsContainer"> 
+		<p class = "center">
+			It looks like your listing is missing some information. Please go <a href="https://jenkins-build.arlcyber.me/uc4-Seller-listing/seller_listing.html">back</a> and enter information into all of the fields. </br> 
+		</p>
 	</div>
 
-</div>
+	<?php
+		}//end of else
+
+		//test
+
+	?> 
+
+
+
 
 
 <div class="footer">
-
 	<p> 
 		Shaheen Reid wrote this file
 	</p>
 </div>
-
 
 </body>
 </html>
