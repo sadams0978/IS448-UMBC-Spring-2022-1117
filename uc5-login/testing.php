76340ddd31f0fb25d842ca862802fcd56ea17ed4
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-14 09:59:47 -0400

    Delete testing.php

--- uc5-login/testing.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	include('../menu.php');
-	if(!isset ($_SESSION['testing_variable'])){
-		 $_SESSION['testing_variable'] = 'This is a test variable. ';
-	}
-	echo ("</br>");
-  echo ("Welcome, " . $_SESSION['testing_variable']); 
-  ?> 
