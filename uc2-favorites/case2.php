10001e0b58995b21105942e340886dedb30f4f6b
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-04-15 11:20:28 -0400

    Update case2.php

--- uc2-favorites/case2.php
+++ uc2-favorites/case2.php
@@ -134,7 +134,7 @@
 				?>
 				<br>
 				<!-- button to remove card from favorites -->
-				<button class = "fa-solid fa-heart" style = "color:red" href = "ListingsView.php"></button>
+				<button href = "ListingsView.php" class = "fa-solid fa-heart" style = "color:red"></button>
 				
 				<button type= "button" onclick="alert('<?php echo 'Card Name: ' . ($card[$i]['C_NAME']) . '\n' . 'Card Description: ' . ($card[$i]['C_DESC']) . '\n' . 'Card Quantity: ' . ($card[$i]['C_QUANTITY']) . 
 				'\n' . 'Card Category: ' . ($card[$i]['C_CATEG']) . '\n' . 'Card Condition: ' . ($card[$i]['C_CONDITION']) . '\n' . 'Card Finish: ' . ($card[$i]['C_FINISH']) . 
