0aa8c442cab586e56d850402caa451f015edb2c2
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-14 23:19:31 -0400

    Update ListingsView.php

--- uc1-Listings-View/ListingsView.php
+++ uc1-Listings-View/ListingsView.php
@@ -1,9 +1,9 @@
 <?php
     include('../db_connection.php');
-    $constructed_query = "INSERT INTO 
-    STOCK(C_NAME, C_DESC, C_QUANTITY, C_CATEG, C_CONDITION, C_FINISH, C_COMP, C_YEAR, PRICE) VALUES
-     ('Charizard Y', 'Special AditionCharizard Y', '7', 'Pokemon', 'Mint', 'Gloss', 'Plastic', '2021', '1.99')";
-	mysqli_query($db, $constructed_query);
+   // $constructed_query = "INSERT INTO 
+   // STOCK(C_NAME, C_DESC, C_QUANTITY, C_CATEG, C_CONDITION, C_FINISH, C_COMP, C_YEAR, PRICE) VALUES
+    // ('Charizard Y', 'Special AditionCharizard Y', '7', 'Pokemon', 'Mint', 'Gloss', 'Plastic', '2021', '1.99')";//
+	//mysqli_query($db, $constructed_query);
 ?>
 <!doctype html>
 <html lang ="en">
