caa63804af183ffb0e44392fc3721716a92e42ad
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-04-14 22:46:30 -0400

    Add files via upload

---
+++ uc2-favorites/case-two-remove1.php
@@ -0,0 +1,29 @@
+<?php
+	session_start();
+?>
+
+<!doctype html>
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<link rel = "stylesheet" type = "text/css" href = "../root.css"/>
+		<link rel = "stylesheet" type = "text/css" href = "uc2-favorites.css"/>
+		<script src="https://kit.fontawesome.com/be0f7619b0.js" crossorigin="anonymous"></script>
+		
+		<title> Favtorites </title>
+	</head>
+	
+	<body>
+		<p class = "cardsContainer">
+			<?Php
+				@$item=$_POST['item'];
+				while (list ($val1) = @each ($item)) {
+					unset($_SESSION['cart'][$val1]);
+				}
+
+
+				require 'case2.php';
+			?>
+		</p>
+	</body>
+</html>
