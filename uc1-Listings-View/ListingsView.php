44464240f76914d21f0a7bc9c13eefe60362a235
GPhillips02 <99154259+GPhillips02@users.noreply.github.com>
2022-04-15 02:00:38 -0400

    Update ListingsView.php

--- uc1-Listings-View/ListingsView.php
+++ uc1-Listings-View/ListingsView.php
@@ -2,7 +2,6 @@
     include('../db_connection.php');
     $constructed_query = "SELECT * FROM STOCK ORDER BY C_ID";
     $results = mysqli_query($db, $constructed_query);
-	$card = mysqli_fetch_array($result);
 ?>
 <!doctype html>
 <html lang ="en">
