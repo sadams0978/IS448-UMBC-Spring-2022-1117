aca36cbf5300562a69b6004fb17572a67ad4ca15
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-04-15 13:15:39 -0400

    Update case2.php

--- uc2-favorites/case2.php
+++ uc2-favorites/case2.php
@@ -115,19 +115,19 @@
 				?>
 				<br>
 				<?php
-				echo $C_NAME;
+				echo $card;
 				?>
 				<br>
 				<?php
-				echo  'Price: ' . $card_price;
+				echo  'Price: ' . ($card['PRICE']);
 				?>
 				<br>
 				<!-- button to remove card from favorites -->
 				<button href = "../index.html" class = "fa-solid fa-heart" style = "color:red"></button>
 				
-				<button type= "button" onclick="alert('<?php echo 'Card Name: ' $C_NAME '\n' 'Card Description: ' $C_DESC '\n' 'Card Quantity: ' $C_QUANTITY
-				'\n' 'Card Category: ' $C_CATEG '\n' 'Card Condition: ' $C_CONDITION '\n' 'Card Finish: ' $C_FINISH 
-				'\n' 'Card Composition: ' $C_COMP '\n' 'Card Year: ' $C_YEAR '\n' 'Card Price: ' $PRICE ?>')">Card Details</button>
+				<button type= "button" onclick="alert('<?php echo 'Card Name: ' ($card['C_NAME']) '\n' 'Card Description: ' ($card['C_DESC']) '\n' 'Card Quantity: ' ($card['C_QUANTITY'])
+				'\n' 'Card Category: ' ($card['C_CATEG']) '\n' 'Card Condition: ' ($card['C_CONDITION']) '\n' 'Card Finish: ' ($card['C_FINISH']) 
+				'\n' 'Card Composition: ' ($card['C_COMP']) '\n' 'Card Year: ' ($card['C_YEAR']) '\n' 'Card Price: ' ($card['PRICE']) ?>')">Card Details</button>
 				</li>
 				
 			</ul>
