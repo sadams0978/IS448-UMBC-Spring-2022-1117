883d80e008ade57f434f37017c5b5dfadef9576f
GPhillips02 <99154259+GPhillips02@users.noreply.github.com>
2022-04-15 04:36:54 -0400

    Update ListingsView.php

--- uc1-Listings-View/ListingsView.php
+++ uc1-Listings-View/ListingsView.php
@@ -108,6 +108,9 @@
 		<li>
 		<?php
 		echo ("<img src='blankcard.jpg'/>");
+		?>
+		<br>
+		<?php
 		echo ($card[$i]['C_NAME']);
 		?>
 		<button type= "button" onclick="alert('<?php echo ($card[$i]['C_NAME']) . ($card[$i]['C_DESC']) . ($card[$i]['C_QUANTITY']) . ($card[$i]['C_CATEG']) . ($card[$i]['C_CONDITION']) . ($card[$i]['C_FINISH']) . ($card[$i]['C_COMP']) . ($card[$i]['C_YEAR']) . ($card[$i]['C_PRICE'])?>')">Card Details</button>
