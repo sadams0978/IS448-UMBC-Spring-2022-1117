c999a972071299c15d85dc595e0ee56f25cc7b4c
RedFalcon9 <99211482+RedFalcon9@users.noreply.github.com>
2022-04-19 18:04:20 -0400

    Update usecase4-submissionpage.php

--- uc4-Seller-listing/usecase4-submissionpage.php
+++ uc4-Seller-listing/usecase4-submissionpage.php
@@ -28,11 +28,11 @@
 			(isset($_POST["condition"]) && (!empty($_POST["condition"]))) &&
 			(isset($_POST["finish"]) && (!empty($_POST["finish"]))) &&
 			(isset($_POST["composition"]) && (!empty($_POST["composition"]))) &&
-			(isset($_POST["year"]) && (!empty($_POST["year"]))) /*&&
-			//(isset($_POST["description"]) && (!empty($_POST["description"]))) &&		//not working atm. play with later if the descirption info is at least posting to db
+			(isset($_POST["year"]) && (!empty($_POST["year"]))) &&
+			(isset($_POST["description"]) && (!empty($_POST["description"]))) &&		//not working atm. play with later if the descirption info is at least posting to db
 			//(isset($_POST["unitprice"]) && (!empty($_POST["unitprice"]))) &&   		//not working atm either
 			//(isset($_POST["sellerquantity"]) && (!empty($_POST["sellerquantity"]))) //&& // not working either
-			 
+			 /*
 			check for image upload. implement later if needed 
 
 			(isset($_POST["cardimage"]) && (!empty($_POST["cardimage"])) */
