e3c931af3e691e1352909829c8386a94f9c89ed8
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-21 11:23:39 -0400

    Update admin.php

--- uc5-login/admin/admin.php
+++ uc5-login/admin/admin.php
@@ -1,20 +1,43 @@
- <?php
+<?php
+include('../../menu.php');
 include('../../db_connection.php');
+$constructed_query = "SELECT email_address, first_name, last_name, FROM login";
 
-    //Selecting the email_address and password from DB
-      $select = "select * from login;";
-      $result = mysqli_query($db, $select);
-	$rows = mysqli_num_rows($result);
-if ($rows > 0) {
-while($row = mysqli_fetch_assoc($result)) {
-        echo $row['email_address']; 
-  }  	
-	
-	
-}
-      
-  mysqli_close($db);
-  die();
+$result = mysqli_query($db, $constructed_query);
 
+		if(! $result){
+			print("There are no users, please check the MySQL Connection");
+			die;
+		}
+
+
+		$num_rows = mysqli_num_rows($result);
+
+		if ($num_rows == 0) {
+		echo ("There are no users, please check the MySQL Connection");
+		die;
+			
+		}
 ?>
 
+	<h3> Users </h3>
+	
+	<table border="2"> <tr>
+	<th> E-Mail Address </th>
+	<th> First Name </th>
+        <th> Last Name </th>
+	</tr>
+
+        <?php
+	  while($db_row = mysqli_fetch_array($result)) {
+		print("<tr>");
+		print("<td> $db_row[email_address] </td>	
+			<td>$db_row[first_name]</td>
+			<td>$db_row[last_name]</td> ");
+		print("</tr>");
+	}		
+		mysqli_close($db);
+		die;
+		?>
+
+</table>
