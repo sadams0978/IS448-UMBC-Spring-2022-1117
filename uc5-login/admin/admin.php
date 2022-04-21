8f063e4b1519bbc4a604c090cbed03e260c53335
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-21 11:03:52 -0400

    Update admin.php

--- uc5-login/admin/admin.php
+++ uc5-login/admin/admin.php
@@ -1,32 +1,13 @@
  <?php
 include('../menu.php');
 include('../db_connection.php');
-$constructed_query = "select email_address, first_name, last_name FROM login;";
-echo ($constructed_query);
-$result = mysqli_query($db, $constructed_query);
-echo ($result);
+$select = "select email_address, first_name, last_name from login"; 
+$result = mysqli_query($db, $select);
 ?>
-
-	<h3> Users </h3>
-	<table border="2">
-	<tr>
-		<th> E-Mail Address </th>
-		<th> First Name </th>
-        	<th> Last Name </th>
-	</tr>
-        
+	<h3> Users </h3>        
         <?php
-	//  while($db_row = mysqli_fetch_array($result))	{
-	//	print("<tr>");
-	//	print("<td>
-		
-	//		$db_row[email_address] </td>
-	//		<td>$db_row[first_name]</td>
- 	  //    		<td>$db_row[last_name]</td> 
-//      ");
-//	print("</tr>");
-//	}		
-//}
+  	while($row = mysqli_fetch_assoc($result)) {
+        echo($row['email_address']);
+        echo($row['first_name']);
+	echo($row['last_name']);
 ?>
-        
-</table>
