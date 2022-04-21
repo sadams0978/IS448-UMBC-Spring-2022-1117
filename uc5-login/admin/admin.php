18d4fe5193f7b090c3518c030bc60af0aea7098b
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-21 10:57:01 -0400

    Update admin.php

--- uc5-login/admin/admin.php
+++ uc5-login/admin/admin.php
@@ -20,16 +20,15 @@ $result = mysqli_query($db, $constructed_query);
 	</tr>
         
         <?php
-//	  while($db_row = mysqli_fetch_array($result))
-//	{
-//		print("<tr>");
-//		print("<td>$db_row[email_address] </td>
-//			<td>$db_row[first_name]</td>
-  //      		<td>$db_row[last_name]</td> 
-    //    ");
-	//	print("</tr>");
-//	}		
-//}
-//?>
+	  while($db_row = mysqli_fetch_array($result))	{
+		print("<tr>");
+		print("<td>$db_row[email_address] </td>
+			<td>$db_row[first_name]</td>
+ 	      		<td>$db_row[last_name]</td> 
+      ");
+	print("</tr>");
+	}		
+}
+?>
         
 </table>
