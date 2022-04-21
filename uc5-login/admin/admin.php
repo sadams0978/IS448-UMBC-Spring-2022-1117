4366668b54c44d42caca6ce38d59a93189a586c4
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-21 10:50:14 -0400

    Update admin.php

--- uc5-login/admin/admin.php
+++ uc5-login/admin/admin.php
@@ -1,7 +1,7 @@
  <?php
 include('../menu.php');
 include('../db_connection.php');
-$constructed_query = "SELECT email_address, first_name, last_name, FROM login";
+$constructed_query = "select "email_address", "first_name", "last_name" FROM login";
 
 $result = mysqli_query($db, $constructed_query);
 
@@ -11,29 +11,23 @@ $result = mysqli_query($db, $constructed_query);
 			print "<p> . $error . </p>";
 			die;
 		}
-
-
-$num_rows = mysqli_num_rows($result);
 ?>
 
-	<h3>
-				Users
-			</h3>
-			<table border="2">
-				<tr>
-					<th> E-Mail Address </th>
-					<th> First Name </th>
-          <th> Last Name </th>
-				</tr>
+	<h3> Users </h3>
+	<table border="2">
+	<tr>
+		<th> E-Mail Address </th>
+		<th> First Name </th>
+        	<th> Last Name </th>
+	</tr>
         
         <?php
 	  while($db_row = mysqli_fetch_array($result))
 	{
 		print("<tr>");
-		print("<td>
-			$db_row[email_address] </td>
-				<td>$db_row[first_name]</td>
-        <td>$db_row[last_name]</td> 
+		print("<td>$db_row[email_address] </td>
+			<td>$db_row[first_name]</td>
+        		<td>$db_row[last_name]</td> 
         ");
 		print("</tr>");
 	}		
