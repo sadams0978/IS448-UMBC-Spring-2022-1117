c5d7139f3214210583bc2c54be4994b104b717ec
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-21 10:45:35 -0400

    Create admin.php

---
+++ uc5-login/admin/admin.php
@@ -0,0 +1,43 @@
+ <?php
+include('../menu.php');
+include('../db_connection.php');
+$constructed_query = "SELECT email_address, first_name, last_name, FROM login";
+
+$result = mysqli_query($db, $constructed_query);
+
+	if(! $result){
+			print("There are no users, please check the MySQL Connection");
+			$error = mysqli_error($db);
+			print "<p> . $error . </p>";
+			die;
+		}
+
+
+$num_rows = mysqli_num_rows($result);
+?>
+
+	<h3>
+				Users
+			</h3>
+			<table border="2">
+				<tr>
+					<th> E-Mail Address </th>
+					<th> First Name </th>
+          <th> Last Name </th>
+				</tr>
+        
+        <?php
+	  while($db_row = mysqli_fetch_array($result))
+	{
+		print("<tr>");
+		print("<td>
+			$db_row[email_address] </td>
+				<td>$db_row[first_name]</td>
+        <td>$db_row[last_name]</td> 
+        ");
+		print("</tr>");
+	}		
+}
+?>
+        
+</table>
