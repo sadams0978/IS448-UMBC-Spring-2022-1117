c2761bc6ae0b30329ea302e1b17628c133e5c27b
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-21 11:15:21 -0400

    Update admin.php

--- uc5-login/admin/admin.php
+++ uc5-login/admin/admin.php
@@ -2,12 +2,11 @@
 include('../db_connection.php');
 
     //Selecting the email_address and password from DB
-      $select = "select * from login";
+      $select = "select * from login;";
       $result = mysqli_query($db, $select);
       
 
   while($row = mysqli_fetch_assoc($result)) {
-
         $db_email = $row['email_address']; 
 	echo ($db_email);   
   }  
