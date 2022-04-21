9254a8463190d2c30e4fb62468fe8b49d1429ea5
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-21 11:28:07 -0400

    Update admin.php

--- uc5-login/admin/admin.php
+++ uc5-login/admin/admin.php
@@ -1,6 +1,6 @@
 <?php
 include('../../db_connection.php');
-	$constructed_query = "SELECT email_address, first_name, last_name, FROM login;";
+	$constructed_query = "SELECT email_address, first_name, last_name FROM login;";
 
 	$result = mysqli_query($db, $constructed_query);
 
