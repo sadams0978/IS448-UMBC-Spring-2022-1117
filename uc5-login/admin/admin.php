c9a291a9a805eec91a20f27514d58e5c8082a000
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-21 11:14:16 -0400

    Update admin.php

--- uc5-login/admin/admin.php
+++ uc5-login/admin/admin.php
@@ -7,17 +7,11 @@ include('../db_connection.php');
       $result = mysqli_query($db, $select);
       
 
-
-  //Checks the matching row's password and e-mail address against the user's input
   while($row = mysqli_fetch_assoc($result)) {
-        $db_first_name = $row['first_name'];
-	$db_last_name = $row['last_name'];
-        $db_email = $row['email_address']; 
-	echo ("The First Name is: " . $db_first_name);
-	echo ("The Last Name is: " . $db_last_name);
-	echo ("The Email is: " . $db_email);   
-  }  
 
+        $db_email = $row['email_address']; 
+	echo ($db_email);   
+  }  
 
 
   mysqli_close($db);
