b1f3b1276024237c45919c98a90ddae4a2663b88
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-21 11:10:21 -0400

    Update admin.php

--- uc5-login/admin/admin.php
+++ uc5-login/admin/admin.php
@@ -1,17 +1,27 @@
  <?php
 include('../menu.php');
 include('../db_connection.php');
-$select = "select email_address, first_name, last_name from login"; 
-$result = mysqli_query($db, $select);
-?>
-	<h3> Users </h3>        
-        <?php
-  	while($row = mysqli_fetch_assoc($result)) {
-        echo($row['email_address']);
-        echo($row['first_name']);
-	echo($row['last_name']);
-	}
 
-  	mysqli_close($db);
-  	die();
+    //Selecting the email_address and password from DB
+      $select = "select email_address, first_name, last_name from login";
+      $result = mysqli_query($db, $select);
+      
+
+
+  //Checks the matching row's password and e-mail address against the user's input
+  while($row = mysqli_fetch_assoc($result)) {
+        $db_first_name = $row['first_name'];
+	$db_last_name = $row['last_name'];
+        $db_email = $row['email_address']; 
+	echo ("The First Name is: " . $db_first_name);
+	echo ("The Last Name is: " . $db_last_name);
+	echo ("The Email is: " . $db_email);   
+  }  
+
+
+
+  mysqli_close($db);
+  die();
+
 ?>
+
