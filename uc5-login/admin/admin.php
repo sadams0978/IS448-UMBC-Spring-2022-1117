58a2f56430b92c32824540f1a7112d5553e52fc0
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-21 10:52:20 -0400

    Update admin.php

--- uc5-login/admin/admin.php
+++ uc5-login/admin/admin.php
@@ -2,15 +2,15 @@
 include('../menu.php');
 include('../db_connection.php');
 $constructed_query = "select 'email_address', 'first_name', 'last_name' FROM login";
+echo ($constructed_query);
+//$result = mysqli_query($db, $constructed_query);
 
-$result = mysqli_query($db, $constructed_query);
-
-	if(! $result){
-			print("There are no users, please check the MySQL Connection");
-			$error = mysqli_error($db);
-			print "<p> . $error . </p>";
-			die;
-		}
+//	if(! $result){
+//			print("There are no users, please check the MySQL Connection");
+//			$error = mysqli_error($db);
+//			print "<p> . $error . </p>";
+//			die;
+//		}
 ?>
 
 	<h3> Users </h3>
@@ -22,16 +22,16 @@ $result = mysqli_query($db, $constructed_query);
 	</tr>
         
         <?php
-	  while($db_row = mysqli_fetch_array($result))
-	{
-		print("<tr>");
-		print("<td>$db_row[email_address] </td>
-			<td>$db_row[first_name]</td>
-        		<td>$db_row[last_name]</td> 
-        ");
-		print("</tr>");
-	}		
-}
-?>
+//	  while($db_row = mysqli_fetch_array($result))
+//	{
+//		print("<tr>");
+//		print("<td>$db_row[email_address] </td>
+//			<td>$db_row[first_name]</td>
+  //      		<td>$db_row[last_name]</td> 
+    //    ");
+	//	print("</tr>");
+//	}		
+//}
+//?>
         
 </table>
