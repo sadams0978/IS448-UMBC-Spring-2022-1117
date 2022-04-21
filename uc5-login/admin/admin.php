d0ae5159dd504326690d065d6a812ac58a5290c2
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-21 10:59:27 -0400

    Update admin.php

--- uc5-login/admin/admin.php
+++ uc5-login/admin/admin.php
@@ -4,11 +4,7 @@ include('../db_connection.php');
 $constructed_query = "select email_address, first_name, last_name FROM login;";
 echo ($constructed_query);
 $result = mysqli_query($db, $constructed_query);
-
-	if(! $result){
-			print("There are no users, please check the MySQL Connection");
-			die;
-		}
+echo ($result);
 ?>
 
 	<h3> Users </h3>
@@ -22,7 +18,9 @@ $result = mysqli_query($db, $constructed_query);
         <?php
 	  while($db_row = mysqli_fetch_array($result))	{
 		print("<tr>");
-		print("<td>$db_row[email_address] </td>
+		print("<td>
+		
+			$db_row[email_address] </td>
 			<td>$db_row[first_name]</td>
  	      		<td>$db_row[last_name]</td> 
       ");
