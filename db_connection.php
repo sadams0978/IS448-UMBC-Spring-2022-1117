f7088ad39390106e5db52535ebea01b5aabebf51
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-04-29 23:58:09 -0400

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -2,7 +2,7 @@
  <?php
 
 // Includes our Database Connection Details
-    	$db = mysqli_connect ("172.17.0.4", "IS448", "IS448password", "samuela3");
-   	if (mysqli_connect_errno()) $db = mysqli_connect ("172.17.0.4", "IS448", "IS448password", "samuela3");
+      	$db = mysqli_connect ("172.17.0.3", "IS448", "IS448password", "samuela3");
+   	if (mysqli_connect_errno())	exit("Error - could not connect to MySQL");
 
 ?>
