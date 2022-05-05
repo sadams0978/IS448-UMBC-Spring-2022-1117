98a6fd39169eb965aff34d2a47bf2ee61530ad26
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-05 13:00:37 -0400

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -40,7 +40,7 @@
 		print("<td> $db_row[email_address] </td>	
 			<td>$db_row[first_name]</td>
 			<td>$db_row[last_name]</td> 
-			<td><button id= '$db_row[email_address]' onclick='buttonPressed()' >Delete</button></td>
+			<td><button id= '$db_row[email_address]' onclick='buttonPressed(this)'>Delete</button></td>
 			");
 		      
 
