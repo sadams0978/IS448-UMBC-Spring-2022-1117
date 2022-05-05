321dbf943e67d355d2b708a96d081c4322b46b3e
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-05 12:55:31 -0400

    Update delete.js

--- admin/delete.js
+++ admin/delete.js
@@ -1,4 +1,5 @@
    function buttonPressed () {
       alert("You have deleted user");
+      document.getElementById("userEmail").innerHTML = "This is my JS!";
       document.getElementById("userEmail").submit();
     }
