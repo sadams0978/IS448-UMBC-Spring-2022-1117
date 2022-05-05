a0598effc17f6f6abaa09976c3a9b37ec6719241
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-05 13:09:18 -0400

    Update delete.js

--- admin/delete.js
+++ admin/delete.js
@@ -1,7 +1,7 @@
    function buttonPressed (emailButton) {
       var emailID = emailButton.id;
       alert("You have deleted user " + emailID);
-      document.getElementByID('userEmail').value = emailID;
+      document.getElementById('userEmail').value = emailID;
       document.getElementById('userEmail').submit();
 
     }
