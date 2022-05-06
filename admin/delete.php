9ff31541287c5f20116d6b61382ebca5dbf92a4d
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-06 11:02:59 -0400

    Update delete.php

--- admin/delete.php
+++ admin/delete.php
@@ -3,10 +3,9 @@ include('../db_connection.php');
 include ('../menu.php');
 $userEmail = $_POST['userEmail'];
 $admin = "index.php";
-$delete = “DELETE FROM login WHERE email_address = ‘$userEmail’”;
+$delete = "DELETE FROM login WHERE email_address = '$userEmail'";
 
-
-mysqli_query($db, $delete));
+mysqli_query($db, $delete);
 header('Location: '. $admin);
 mysqli_close($db);
 
