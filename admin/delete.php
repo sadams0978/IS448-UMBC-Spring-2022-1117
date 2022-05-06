1534ab07eebdb54f1a4f994757390114f7c21d25
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-06 10:54:09 -0400

    Update delete.php

--- admin/delete.php
+++ admin/delete.php
@@ -1,6 +1,15 @@
 <?php
+include('../db_connection.php');
+include ('../menu.php');
 $userEmail = $_POST['userEmail'];
+$admin = "index.php";
+$delete = “DELETE FROM login WHERE email_address = ‘$userEmail’”;
 
-echo ($userEmail);
+
+if (mysqli_query($db, $delete)) {
+       header('Location: '. $admin);
+
+} else echo ("We couldn't make the change, please try again. ");
+
 
 ?>
