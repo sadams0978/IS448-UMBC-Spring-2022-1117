c7b0f18edd75f58b2921624cbc89ac7c0366e1fb
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-06 11:41:11 -0400

    Update delete.php

--- admin/delete.php
+++ admin/delete.php
@@ -1,28 +1,20 @@
 <?php
-
-
 	//Only Authorized Users can access this admin page
-	$login = "../uc5-login/index.php";
 	$404 = "../404.html";
 	session_start(); 
-	if(!isset ($_SESSION['email'])){
-		header('Location: '. $login); 
-	}
 	
 	if ($_SESSION['email'] !== "sam@arlcyber.me") {
 	header("HTTP/1.0 404 Not Found");
 	header('Location: '. $404); 
 	die();
 	}
-
-
 	
-include('../db_connection.php');
-$userEmail = $_POST['userEmail'];
-$admin = "index.php";
-$delete = "DELETE FROM login WHERE email_address = '$userEmail'";
+	include('../db_connection.php');
+	$userEmail = $_POST['userEmail'];
+	$admin = "index.php";
+	$delete = "DELETE FROM login WHERE email_address = '$userEmail'";
 
-mysqli_query($db, $delete);
-header('Location: '. $admin);
-mysqli_close($db);
+	mysqli_query($db, $delete);
+	header('Location: '. $admin);
+	mysqli_close($db);
 ?>
