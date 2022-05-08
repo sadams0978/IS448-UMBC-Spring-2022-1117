29a19ea7e2af7e7b7b8f681b4fb957fd88e48304
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-08 11:10:05 -0400

    Update verify.js

--- uc5-login/verify.js
+++ uc5-login/verify.js
@@ -1,19 +1,23 @@
-window.onload = pageLoad;
+document.getElementById("password").onchange = function() {checkPassword()};
 
-function pageLoad(){
-	document.getElementById("password").onchange = checkPassword;
+function myFunction() {
+  var x = document.getElementById("fname");
+  x.value = x.value.toUpperCase();
 }
 
 
 
 
+
 function checkPassword () {
+var pagePassword = document.getElementById("password");
+var userPassword = pagePassword.value;
  var contains_uppercase = /[A-Z]+/; 
  var contains_special_char = /\W+/;
  var contains_digit = /\d+/;
  var userPassword;
  
- document.getElementById('password').value = userPassword;
+
   
   
  var result1 = contains_uppercase.test(userPassword);
@@ -22,11 +26,18 @@ function checkPassword () {
   
   if (result1 && result2 && result3) {
     
-   document.getElementById('password').style.color="green"; 
+   pagePassword.style.color="green"; 
    return true;
 
     
-  } else return false;
+  } else  {
+	  
+pagePassword.style.color="red";
+	  
+	  return false;
+  }
+	  
+	  
   
  
   
