4552eb7e0558ec6215a7e52cb65960cbbf312b66
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-08 10:54:14 -0400

    Create verify.js

---
+++ uc5-login/verify.js
@@ -0,0 +1,20 @@
+function checkPassword (password) {
+ var contains_uppercase = /[A-Z]+/; 
+ var contains_special_char = /\W+/;
+ var contains_digit = /\d+/;
+ var userPassword = password.value;
+  
+  
+ var result1 = contains_uppercase.test(userPassword);
+ var result2 = contains_special_char.test(userPassword);
+ var result3 = contains_digit.test(userPassword);
+  
+  if (result1 && result2 && result3) {
+    
+   password.style.color="green"; 
+    
+  }
+  
+ 
+  
+}
