591278437093338d6d4556bfb3945f44dfc5e366
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-09 18:27:22 -0400

    Update visable.js

--- uc5-login/visable.js
+++ uc5-login/visable.js
@@ -1,8 +1,10 @@
-function visibility(passwordField) {
+function visibility(passwordField, buttonField) {
   if (passwordField.type === "password") {
  	  passwordField.type = "text";
+    buttonField.className = "fa-solid fa-eye-slash";
   } else {
     passwordField.type = "password";
+    buttonField.className = "fa-solid fa-eye";
   }
 }
 
