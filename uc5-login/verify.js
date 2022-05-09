8e52608dbb8cfc6a0f70b12482e1c1b2e6bb296b
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-09 19:14:57 -0400

    Update verify.js

--- uc5-login/verify.js
+++ uc5-login/verify.js
@@ -1,34 +1,47 @@
-document.getElementById("password").oninput = function() {checkPassword()};
+document.getElementById("password").onFocus = function() {checkPassword()};
 
 
 function checkPassword () {
+
+
 var pagePassword = document.getElementById("password");
+
 var userPassword = pagePassword.value;
  var contains_uppercase = /[A-Z]+/; 
  var contains_special_char = /\W+/;
  var contains_digit = /\d+/;
- var userPassword;
  
 
+  	//Result Checking for conditions
   
-  
- var result1 = contains_uppercase.test(userPassword);
- var result2 = contains_special_char.test(userPassword);
- var result3 = contains_digit.test(userPassword);
-  
-  if (result1 && result2 && result3) {
-    
-   pagePassword.style.color="green"; 
-   return true;
-
-    
-  } else  {
+	var result1 = contains_uppercase.test(userPassword);
+	var result2 = contains_special_char.test(userPassword);
+	var result3 = contains_digit.test(userPassword);
+	var passworLength = userPassword.length;
+	
+	
+	
+	
+	if (result1) {
+	document.getElementById("upper_req").style.color="green"; 
+	} 
+	
+	if (result2) {
+	document.getElementById("special_req").style.color="green"; 	
+	}
+	
+	if (result3) {	
+	document.getElementById("digit_req").style.color="green"; 	
+	}
+	
+	if (passwordLength >= 8) {
+	document.getElementById("char_req").style.color="green"; 	
+			
+	}
+	
 	  
-pagePassword.style.color="red";
-	  
-	  return false;
-  }
-	  
+	
+	
 	  
   
  
