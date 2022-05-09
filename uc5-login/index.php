80ba89d0f90be9141c8aca1041854daaefd78956
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-09 09:15:01 -0400

    Update index.php

--- uc5-login/index.php
+++ uc5-login/index.php
@@ -15,7 +15,7 @@
 function visibility() {
   var x = document.getElementById("userPassword");
   if (x.type === "password") {
-    x.type = "text";
+ 	  x.type = "text";
   } else {
     x.type = "password";
   }
