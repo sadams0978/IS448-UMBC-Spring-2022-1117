3b7899841a90a6f4f187282a7b19958bf6808564
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-10 12:48:42 -0400

    Update change_password.php

--- uc5-login/change_password.php
+++ uc5-login/change_password.php
@@ -10,6 +10,16 @@
 </head>
 
 <body>
+	<div class="password_requirements" id="password_requirements" style="visibility: hidden;" >
+	
+	<h3 style="color:black"> Password Requirements: </h3>
+		<p id="char_req" > 8 or more characters </p>
+		<p id="upper_req" > 1 Uppercase letter </p>
+		<p id="digit_req" > 1 Digit </p>
+		<p id="special_req" > 1 Special Character </p>
+		
+	</div>
+	
     <div class="Login_Box">
 
     <h1> Change Your Password: </h1>
@@ -25,12 +35,12 @@
     <button type="button" onclick="visibility(old_password)"> <i class="fa-solid fa-eye"></i> </button>
 
     
-    <input type="password" name="password" id="password" placeholder="New Password:" required>
+    <input type="password" name="password" id="password" placeholder="New Password:" onfocus="visible()" onblur="hide()" onchange="checkPassword()">
       
     <button type="button" onclick="visibility(password)"> <i class="fa-solid fa-eye"></i> </button>
 
     
-    <input type="password" name="password_verify" id = "password_verify" placeholder="Verify your New Password:" required >
+    <input type="password" name="password_verify" id = "password_verify" placeholder="Verify your New Password:" >
       
      <button type="button" onclick="visibility(password_verify)"> <i class="fa-solid fa-eye"></i> </button>
     
@@ -48,5 +58,7 @@
     <h3> <strong> <a href="https://www.umbc.edu/policies/pdfs/UMBC%20X1.00.06%20UMBC%20Policy%20on%20Web%20Site%20Privacy%20Statement.pdf#:~:text=It%20is%20university%20policy%20that,sites%20not%20affiliated%20with%20UMBC." class = "Site_Link"> Privacy Policy</a> </strong> </h3>
    </form>
   </div>
+	    <script src="verify.js"></script>
+
 </body>
 </html>
