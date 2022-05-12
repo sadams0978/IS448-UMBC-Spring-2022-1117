d163ac5138332ff81cea14e522013b7eaca761f7
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-11 21:48:59 -0400

    Create test.php

---
+++ admin/test.php
@@ -0,0 +1,21 @@
+<?php
+
+$group_result = 'user';
+//If User Group isn't set, check the DB and update the session variable
+	if(!isset ($_SESSION['group'])){
+	
+	include('../db_connection.php');
+	
+	$email = $_SESSION['email'];
+		
+	
+	$select = "select Member from login where email_address = '$email'"; 
+	$group_result = mysqli_query($db,$select);
+    	$_SESSION['group'] = $group_result;
+	mysqli_close();
+	}
+
+  echo ($group_result);
+
+
+?>
