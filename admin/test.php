8893c37c4cd006a13143788db179db2468741f72
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-11 22:01:31 -0400

    Update test.php

--- admin/test.php
+++ admin/test.php
@@ -1,23 +1,22 @@
 <?php
+include('../menu.php');
 
 //$group_result = 'user';
 //If User Group isn't set, check the DB and update the session variable
-	//if(!isset ($_SESSION['group'])){
-	
-	//include('../db_connection.php');
-	include('../menu.php');
-
+	if(!isset ($_SESSION['group'])){
 	$email = $_SESSION['email'];
 		
-	echo ($email);
+	include('../db_connection.php');
+	$select = "select Member from login where email_address = '$email'"; 
 		
-	//$select = "select Member from login where email_address = '$email'"; 
-	//$group_result = mysqli_query($db,$select);
-    	//$_SESSION['group'] = $group_result;
-	//mysqli_close();
-	//}
+		
+	echo ($select);
+	$group_result = mysqli_query($db,$select);
+    	$_SESSION['group'] = $group_result;
+	mysqli_close();
+	}
 
-  //echo ($select);
+  echo ("We have made it past with the group of " . $_SESSION['group']);
 
 
 ?>
