85d644d40f89e572fa6ca0cf18ca827be55dd0e0
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-12 10:14:09 -0400

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -7,6 +7,7 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="../root.css"/>
 	<script src="https://kit.fontawesome.com/be0f7619b0.js" crossorigin="anonymous"></script>
+	<script type="text/javascript" src=" https://ajax.googleapis.com/ajax/libs/prototype/1.7.3.0/prototype.js"></script>
 	<script src="delete.js"></script>
 
     <title>Admin</title>
