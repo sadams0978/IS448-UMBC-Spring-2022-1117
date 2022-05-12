aecb6945542f63c4594369e6f0ce38678fb9d836
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-11 21:56:45 -0400

    Update test.php

--- admin/test.php
+++ admin/test.php
@@ -10,13 +10,13 @@
 		
 	echo ($email);
 		
-	$select = "select Member from login where email_address = '$email'"; 
+	//$select = "select Member from login where email_address = '$email'"; 
 	//$group_result = mysqli_query($db,$select);
     	//$_SESSION['group'] = $group_result;
 	//mysqli_close();
 	//}
 
-  echo ($select);
+  //echo ($select);
 
 
 ?>
