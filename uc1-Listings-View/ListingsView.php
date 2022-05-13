81c1d12939c511c8bd2c4f1a9120feeb82d35987
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-13 14:57:45 -0400

    Update ListingsView.php

--- uc1-Listings-View/ListingsView.php
+++ uc1-Listings-View/ListingsView.php
@@ -120,7 +120,9 @@
 		?>
 		<br>
 			
-		<button href = "case2.php?C_ID='.$card[$i]['C_ID'].'" onclick = "alert('Added to Favorites')">Favorite</button>	
+		<!-- <button href = "case2.php?C_ID='.$card[$i]['C_ID'].'" onclick = "alert('Added to Favorites')">Favorite</button> -->	
+			
+		<button href = "(<?php echo (case2.php?C_ID=$card[$i]['C_ID']); ?>)" onclick = "alert('Added to Favorites')">Favorite</button>	
 			
 		<button type= "button" onclick="alert('<?php echo 'Card Name: ' . ($card[$i]['C_NAME']) . '\n' . 'Card Description: ' . ($card[$i]['C_DESC']) . '\n' . 'Card Quantity: ' . ($card[$i]['C_QUANTITY']) . '\n' . 'Card Category: ' . ($card[$i]['C_CATEG']) . '\n' . 'Card Condition: ' . ($card[$i]['C_CONDITION']) . '\n' . 'Card Finish: ' . ($card[$i]['C_FINISH']) . '\n' . 'Card Composition: ' .($card[$i]['C_COMP']) . '\n' . 'Card Year: ' .($card[$i]['C_YEAR']) . '\n' . 'Card Price: ' . ($card[$i]['PRICE'])?>')">Card Details</button>
 		</li>
