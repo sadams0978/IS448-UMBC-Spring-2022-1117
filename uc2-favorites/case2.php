e320679465e49092111bf8472bca5f694060f5d9
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-13 16:51:00 -0400

    Update case2.php

--- uc2-favorites/case2.php
+++ uc2-favorites/case2.php
@@ -40,7 +40,7 @@
 <!doctype html>
 <html>
 	<head>
-		<meta charset = "utf-8" content = "1;url = ListingsView.php"/>
+		<meta charset = "utf-8">
 		<link rel = "stylesheet" type = "text/css" href = "../root.css"/>
 		<link rel = "stylesheet" type = "text/css" href = "uc2-favorites.css"/>
 		<script src="https://kit.fontawesome.com/be0f7619b0.js" crossorigin="anonymous"></script>
