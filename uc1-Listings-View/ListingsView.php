633dc17f7ab534bc4ef9b0e2866352a66cee4ea0
GPhillips02 <99154259+GPhillips02@users.noreply.github.com>
2022-05-13 18:18:22 -0400

    Update ListingsView.php

--- uc1-Listings-View/ListingsView.php
+++ uc1-Listings-View/ListingsView.php
@@ -14,10 +14,98 @@
 </head>
 <body>
 	
+<p class="spacer"></p>
+	<div class ="cardDetailsContainer"> 
+	<h2> Filters </h2>
+        <form action="ListingsView.php" method="POST" name="Filter">
+		
+        <!--Enter card category --> 
+		<label for="category"> Category:</label> 
+        <!--from sql & php-->
+			<select name= "category" id="category"> 
+				<option value="*" disabled selected>Select Category</option> 
+        <!-- acts as place holder prior to user interacting with element--> 
+				<option value="Pokemon">Pokemon</option>
+				<option value="Yu Gi Oh">Yu Gi Oh</option>
+				<option value="Baseball">Baseball</option>
+				<option value="Football">Football</option>
+				<option value="Hockey">Hockey</option>
+				<option value="Other">Other</option>
+			</select>
+		<br/>
+
+		<!--Enter card condition--> 
+		<label for="condition"> Condition:</label> 
+        <!--from sql & php-->
+			<select name= "condition" id="condition"> 
+				<option value="*" disabled selected>Select Condition</option>
+				<option value="Poor">Poor</option>
+				<option value="Average">Average</option>
+				<option value="Great">Great</option>
+				<option value="Mint">Mint</option>
+			</select>
+		<br/> 
+
+		<!--Enter card finish--> 
+		<label for="finish"> Finish:</label> 
+        <!--from sql & php-->
+			<select name= "finish" id="finish"> 
+				<option value="*" disabled selected>Select Finish</option>
+				<option value="Matte">Matte</option>
+				<option value="Satin">Satin</option>
+				<option value="Gloss">Gloss</option>
+				<option value="Other">Other</option>
+			</select>
+		<br/>
+
+		<!-- Enter card composition--> 
+		<label for="composition"> Composition:</label> 
+        <!--from sql & php-->
+			<select name= "composition" id="composition"> 
+				<option value="*" disabled selected>Select Composition</option>
+				<option value="Paper Board">Paper Board</option>
+				<option value="Thick Paper">Thick Paper</option>
+				<option value="Plastic">Plastic</option>
+				<option value="Metal">Metal</option>
+				<option value="Other">Other</option>
+			</select>
+            </br>
+
+        <!-- Enter card Year--> 
+		<label for="year"> Year:</label> 
+        <!--from sql & php-->
+			<select name= "year" id="year"> 
+				<option value="*" disabled selected>Select Year</option>
+				<option>Select Year</option>
+            	<?php
+                for($year = 1980; $year <=2022; $year++){
+                echo "<option value=".$year.">".$year."</option>";
+            }
+            ?>
+			</select>
+            </br>
+
+            <input type="submit" value="Submit"/>
+        </form>
+        <?php
+            echo "Your current filters are:";
+            echo '\n';
+            echo "Category: " . $_SESSION['category'] ;
+            echo '\n';
+            echo "Condition: " . $_SESSION['condition'];
+            echo '\n';
+            echo "Finish: " . $_SESSION['finish'];
+            echo '\n';
+            echo "Composition: " . $_SESSION['composition'];
+            echo '\n';
+            echo "Year: " . $_SESSION['year'];
+        ?>
+	</div>
+    <!--FILTER Section Over--> 
+
 	<?php
 	// Includes our menu bar, instead of copying and pasting through the pages
 	include('../menu.php');
-	include('../filter.php');
 	$_SESSION['category'] = $_POST["category"];
 	$_SESSION['condition'] = $_POST["condition"];
 	$_SESSION['finish'] = $_POST["finish"];
