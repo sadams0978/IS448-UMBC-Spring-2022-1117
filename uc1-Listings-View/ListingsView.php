183ea4944e8c1920221fa3f5459adad96c7c90b9
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-13 20:39:46 -0400

    Update ListingsView.php

--- uc1-Listings-View/ListingsView.php
+++ uc1-Listings-View/ListingsView.php
@@ -159,7 +159,7 @@
 		<!-- favorites button -->
 		<form action="case2.php" method="POST" name="Favorites">
 			<input type = "hidden" name="C_ID" value="<?php echo $card[$i]['C_ID'];?>">
-			<button type="submit" value="Favorite"/>
+			<button type="submit">Favorite</button>
 		</form>
 
 		<button type= "button" onclick="alert('<?php echo 'Card Name: ' . ($card[$i]['C_NAME']) . '\n' . 'Card Description: ' . ($card[$i]['C_DESC']) . '\n' . 'Card Quantity: ' . ($card[$i]['C_QUANTITY']) . '\n' . 'Card Category: ' . ($card[$i]['C_CATEG']) . '\n' . 'Card Condition: ' . ($card[$i]['C_CONDITION']) . '\n' . 'Card Finish: ' . ($card[$i]['C_FINISH']) . '\n' . 'Card Composition: ' .($card[$i]['C_COMP']) . '\n' . 'Card Year: ' .($card[$i]['C_YEAR']) . '\n' . 'Card Price: ' . ($card[$i]['PRICE'])?>')">Card Details</button>
