c909c23f698092865899df569ee3c6883a040184
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-19 18:51:51 -0400

    Update ListingsView.php

--- uc1-Listings-View/ListingsView.php
+++ uc1-Listings-View/ListingsView.php
@@ -163,7 +163,7 @@
 		<?php 
 		$_SESSION['c_id'] = $card[$i]['C_ID'];	
 		?>
-		<input type = "hidden" id = "c_id" name="c_id" value="<?php echo $card[$i]['C_ID'];?>"/>
+		<input type = "hidden" id = "c_id" name="c_id" value=""/>
 		<input type = "submit" id = "submitFav" value = "Favorite"/>
 		
 
