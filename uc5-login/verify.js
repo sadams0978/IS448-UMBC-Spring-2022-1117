61f050f3965109ed346bd1f4a7d2da3b98ef1f53
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-20 19:51:17 -0400

    Update verify.js

--- uc5-login/verify.js
+++ uc5-login/verify.js
@@ -118,12 +118,10 @@ function checkEmail () {
 	console.log(isEmail);
 	var emailResult = isEmail.test(email);
 	
-	
-	alert ("Please check your email address.");	
 	if (emailResult) {
 	return true;
 	} else  {
-	
+	alert ("Please check your email address.");
 		return false;
 	}
 	
