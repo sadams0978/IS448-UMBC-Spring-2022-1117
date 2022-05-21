fcebdbbd5687202b06ea49a159dfd49369f79603
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-21 19:05:54 -0400

    Update visable.js

--- uc5-login/visable.js
+++ uc5-login/visable.js
@@ -14,3 +14,16 @@ function clearField () {
 }
 
 
+
+function icon (iconName) {
+ if ( iconName.classList.contains('fa-eye') ) {
+   iconName.classList.remove('fa-eye');
+   iconName.classList.add('fa-eye-slash');
+  
+} else  {
+ iconName.classList.add('fa-eye');
+ iconName.classList.remove('fa-eye-slash');
+   
+}
+
+
