bcfda6e0e209107b6b67232d32dc366263cb6010
RedFalcon9 <99211482+RedFalcon9@users.noreply.github.com>
2022-05-21 17:21:30 -0400

    Add files via upload

--- uc4-Seller-listing/usecase4.js
+++ uc4-Seller-listing/usecase4.js
@@ -2,21 +2,64 @@
 
 window.onload = pageLoad; 
 function pageLoad(){
+	document.getElementById("submitB").onclick = checkForm; 
+
 	document.getElementById("biggerTextB").onclick=increaseTextSize;
 	document.getElementById("smallerTextB").onclick=decreaseTextSize;
 
 }
 
-//modifys the text size within the text box to increase 
+//modifies the text size within the text box to increase 
 function increaseTextSize () { 
 	var tarea = document.getElementById("description");
 	tarea.style.fontSize = "24pt";
-	alert("You have pressed the increase button"); 
 }
 
-//modifys the text size within the text box to decrease 
+//modifies the text size within the text box to decrease 
 function decreaseTextSize () { 
 	var tarea = document.getElementById("description");
-	tarea.style.fontSize = "5pt";
-	alert("You have pressed the increase button"); 
+	tarea.style.fontSize = "10pt"; 
+}
+
+function checkForm () {
+
+	var card_name = document.getElementById("card_name"); 
+	var card_year = document.getElementById("year");
+	var card_category = document.getElementById("category");
+
+//checks to ensure that fields have not been left empty 
+	if(card_name == ""){
+		alert("You did not enter a card name. Please enter one now.");
+		document.getElementById("year").focus();
+		return false; 
+	}
+	if(card_year == ""){
+		alert("You did not enter a card year. Please enter one now.");
+		document.getElementById("year").focus();
+		return false;
+	}
+	if(card_category == ""){
+		alert("cateogry empty");
+		document.getElementById("category").focus();
+		return false; 
+	}
+	if(card_year == "Select your option"){
+		alert(" select your option is stored");
+		document.getElementById("category").focus();
+		return false;
+	}
+
+
+
+//checks and alerts user to enter a year as 4 digits
+	var card_year_pattern = /\d{4}/; 
+	var card_year_pattern_result = card_year_pattern.test(card_year); 
+	if (result1 == false){
+		alert("The year you entered (" + card_name + ") is not valid. Please enter a four digit year (example: 1999)  ");
+		document.getElementById("year").focus(); 
+		return false; 
+	}
+
+
+
 }
