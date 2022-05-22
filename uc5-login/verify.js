69c837eda3a70c3612acebb38a3b409ab9da9d55
Sam Adams <59022873+sadams0978@users.noreply.github.com>
2022-05-22 12:15:38 -0400

    Update verify.js

--- uc5-login/verify.js
+++ uc5-login/verify.js
@@ -47,7 +47,7 @@ function checkPassword () {
 	
 		if ( (result1 == true) && (result2 == true) && (result3 == true) && (result4 == true) ) {
 			passwordRequirements == true;
-			document.getElementById("password").style.border = "none";
+			document.getElementById("password").style.border = "black";
 			return true;
 			
 		} else {
