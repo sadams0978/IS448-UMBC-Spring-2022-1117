54d8558e86a6236c98297b94d4296a18321ac6c5
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-22 22:00:41 -0400

    Update case2.php

--- uc2-favorites/case2.php
+++ uc2-favorites/case2.php
@@ -43,12 +43,16 @@
 		$year = "AND C_YEAR = '$_SESSION[year]'";
 	}
 		
-	if(isset($_SESSION['favorites'])){
-		foreach($_SESSION['favorites'] as $item){
-			$sql = 'SELECT * FROM STOCK WHERE C_ID ='.$item;
-			$result = mysqli_query($db,$sql);
-			$card = mysqli_fetch_array($result);
-		}
+// 	if(isset($_SESSION['favorites'])){
+// 		foreach($_SESSION['favorites'] as $item){
+// 			$sql = 'SELECT * FROM STOCK WHERE C_ID ='.$item;
+// 			$result = mysqli_query($db,$sql);
+// 			$card = mysqli_fetch_array($result);
+// 		}
+// 	}
+	$constructed_query = "SELECT * FROM STOCK WHERE C_ID < 1000 $category $condition $finish $composition $year";
+	$result = mysqli_query($db, $constructed_query);
+	for ($card = array (); $row = $result->fetch_assoc(); $card[] = $row){
 	}
 	?>
     	
