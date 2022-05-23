f745b676c797707c134fa8d9bdc3e5ecba7589db
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 15:55:33 -0400

    Update case2.php

--- uc2-favorites/case2.php
+++ uc2-favorites/case2.php
@@ -139,9 +139,14 @@
         ?>
 	</div>
 	<div class = "cardsContainer">
-			$sql = 'SELECT * FROM STOCK WHERE C_ID ='.$_SESSION['favorites'][$c_ID];
-			$result1 = mysqli_query($db,$sql);
-			$card1 = mysqli_fetch_array($result1);
+		<?php
+		if(isset($_SESSION['favorites'])){
+			foreach($_SESSION['favorites'] as $item){
+				$sql = 'SELECT * FROM STOCK WHERE C_ID='.$item;
+				$result1 = mysqli_query($db,$sql);
+				$card1 = mysqli_fetch_array($result1);
+			
+		?>	
 		
 		<section>
 			<img src='blank-card.jpg' width = '150' height = '250'/>
@@ -157,7 +162,19 @@
 			<?php
 			echo ($card1['C_ID']);	
 			?>
+			<br>
+			<!-- button to remove card from favorites -->	
+			<?php
+			echo '<a href = "../uc1-Listings-View/ListingsView.php"><button class = "fa-regular fa-heart" style = "color:red"></button></a>';
+			?>
+			
+			<button type= "button" onclick="alert('<?php echo 'Card Name: ' . ($card1['C_NAME']) . '\n' . 'Card Description: ' . ($card1['C_DESC']) . '\n' . 'Card Quantity: ' . ($card1['C_QUANTITY']) .
+			'\n' . 'Card Category: ' . ($card1['C_CATEG']) . '\n' . 'Card Condition: ' . ($card1['C_CONDITION']) . '\n' . 'Card Finish: ' . ($card1['C_FINISH']) . 
+			'\n' . 'Card Composition: ' . ($card1['C_COMP']) . '\n' . 'Card Year: ' . ($card1['C_YEAR']) . '\n' . 'Card Price: ' . ($card1['PRICE'])?>')">Card Details</button>
+				
 		</section>
+		<?php } ?>
+		<?php } ?>
 	</div>
 		
 		<p class = "spacer"></p>
