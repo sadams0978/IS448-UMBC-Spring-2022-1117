d76593ab2bd69f5be1e5c99e64dde1c3d2789aed
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 11:08:23 -0400

    Update add_favorite.php

--- uc2-favorites/add_favorite.php
+++ uc2-favorites/add_favorite.php
@@ -1,6 +1,7 @@
 <?php
-	include('../menu.php');
-  $addFav = "../uc1-Listings-View/ListingsView.php";
+	session_start();
+	include('../db_connection.php');
+  	$addFav = "../uc1-Listings-View/ListingsView.php";
 	//check if card is already in favorites
 	if(!in_array($_GET['C_ID'], $_SESSION['favorites'])){
 		array_push($_SESSION['favorites'], $_GET['C_ID']);
