8e867105a6482f42d872b06e8a473c81df4a5bd9
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 19:05:41 -0400

    Update case2.php

--- uc2-favorites/case2.php
+++ uc2-favorites/case2.php
@@ -175,7 +175,7 @@
 						?>
 						<br>
 						
-						<a href = "delete_card.php?C_ID=<?php echo $card1['C_ID']; ?>&index=<?php echo $index; ?>" class="fa-regular fa-trash-can" style="color:black" style="background:none"></a>
+						<a href = "delete_card.php?C_ID=<?php echo $card1['C_ID']; ?>&index=<?php echo $index; ?>" class="fa-regular fa-trash-can"></a>
 						
 						<input type="hidden" name="indexes[]" value="<?php echo $index; ?>">
 						
