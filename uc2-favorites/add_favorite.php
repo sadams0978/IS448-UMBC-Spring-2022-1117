a9533b3230bfaef9ffa4f01bbb37d65cdbebddf2
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 03:26:49 -0400

    Update add_favorite.php

--- uc2-favorites/add_favorite.php
+++ uc2-favorites/add_favorite.php
@@ -1,12 +1,33 @@
 <?php
-  include('../db_connection.php');
-  include('../menu.php');
-
-  $add = "../uc1-Listings-View/ListingsView.php";
-  if(!in_array($_GET['C_ID'], $_SESSION['favorites'])){
-    array_push($_SESSION['favorites'], $_GET['C_ID']);
-    $_SESSION['message'] = 'Card added to Favorites';
-  }
-
-  header('location: '. $add);
+	include('../db_connection.php');
 ?>
+<!doctype html>
+<html>
+	<head>
+		<meta charset = "utf-8" http-equiv="refresh" content="1;url=../uc1-Listings-View/ListingsView.php"/>
+		<link rel = "stylesheet" type = "text/css" href = "../root.css"/>
+		<link rel = "stylesheet" type = "text/css" href = "uc2-favorites.css"/>
+		<script src="https://kit.fontawesome.com/be0f7619b0.js" crossorigin="anonymous"></script>
+		
+		<title> Add Favorite </title>
+	</head>
+	
+	<body>
+    <?php
+    include('../menu.php'):
+    ?>
+    <h1>Added To Favorites</h1>
+    <?php
+    $C_ID = $_GET['C_ID'];
+    
+    if(!isset($_SESSION['favorites'])){
+        $favoritesArr = array();
+        $_SESSION['favorites'] = $favoritesArr;
+    }
+    
+    array_push($_SESSION['favorites'],$C_ID);
+    ?>
+  
+    
+  </body>
+</html>
