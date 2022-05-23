3248b46944fb08c3bd5aaf5dc2561c2919b5aecc
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 18:05:12 -0400

    Update delete_card.php

--- uc2-favorites/delete_card.php
+++ uc2-favorites/delete_card.php
@@ -5,6 +5,8 @@
 	$key = array_search($_GET['C_ID'], $_SESSION['favorites']);	
 	unset($_SESSION['cart'][$key]);
 
+	unset($_SESSION['qty_array'][$_GET['index']]);
+	$_SESSION['qty_array'] = array_values($_SESSION['qty_array']);
 
 	$_SESSION['message'] = "Product deleted from cart";
 	header('location: case2.php');
