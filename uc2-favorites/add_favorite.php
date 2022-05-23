d036502ad6c9fbfd56e56a463a3c5e07bed616d1
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 10:04:17 -0400

    Create add_favorite.php

---
+++ uc2-favorites/add_favorite.php
@@ -0,0 +1,14 @@
+<?php
+	session_start();
+  $addFav = "../uc1-Listings-View/ListingsView.php";
+	//check if card is already in favorites
+	if(!in_array($_GET['C_ID'], $_SESSION['favorites'])){
+		array_push($_SESSION['favorites'], $_GET['C_ID']);
+		$_SESSION['message'] = 'Card added to favorites';
+	}
+	else{
+		$_SESSION['message'] = 'Card already in favorites';
+	}
+ 
+	header('location: '.$addfav);
+?>
