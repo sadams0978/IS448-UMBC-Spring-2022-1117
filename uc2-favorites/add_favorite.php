641251af7202df0c0b36ceed05be53c648ee0614
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 18:23:07 -0400

    Update add_favorite.php

--- uc2-favorites/add_favorite.php
+++ uc2-favorites/add_favorite.php
@@ -1,33 +1,14 @@
-<!doctype html>
-<html lang ="en">
-<head>
-    
-    <meta http-equiv="refresh" content="1;url=../uc1-Listings-View/ListingsView.php"/>
-    <link rel="stylesheet" type="text/css" href="../root.css"/>
-    <link rel="stylesheet" type="text/css" href="uc2-favorites.css"/>
-    
-</head>
-<body>
-	<?php
-    	include('../db_connection.php');
-	?>
-	<?php
-	include('../menu.php');
-	?>
-	<h1>Added To Favorites</h1>
-	<?php
-		$c_ID = $_GET['C_ID'];
+<?php
+	session_start();
+	//check if card in favorites
+	if(!in_array($_GET['C_ID'], $_SESSION['favorites'])){
+		array_push($_SESSION['favorites'], $_GET['C_ID']);
+		$_SESSION['message'] = 'Card Added to Favorites';
+	}
+	else{
+		$_SESSION['message'] = 'Card Already in Favorites';
+	}
 	
-		if(!isset($_SESSION['favorites'])){
-			$favoritesArr = array();
-			$_SESSION['favorites'] = $favoritesArr;
-		}
-		
-		unset($_SESSION['qty_array']);
-	
-		array_push($_SESSION['favorites'],$c_ID);
-		//echo $_SESSION['favorites'][$c_ID];
-	?>
-	
-</body>
-</html>
+	$msglocation = "../uc1-Listings-View/ListingsView.php";
+	header('location: '. $msglocation);
+?>
