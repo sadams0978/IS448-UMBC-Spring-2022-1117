bdb1ab2ec1570f4287b27cd170e008d9bc7b69fe
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 19:26:16 -0400

    Update case2.php

--- uc2-favorites/case2.php
+++ uc2-favorites/case2.php
@@ -43,10 +43,10 @@
 		$year = "AND C_YEAR = '$_SESSION[year]'";
 	}
 	
-	$constructed_query = "SELECT * FROM STOCK WHERE C_ID < 1000 $category $condition $finish $composition $year";
-	$result = mysqli_query($db, $constructed_query);
-	for ($card = array (); $row = $result->fetch_assoc(); $card[] = $row){
-	}
+// 	$constructed_query = "SELECT * FROM STOCK WHERE C_ID < 1000 $category $condition $finish $composition $year";
+// 	$result = mysqli_query($db, $constructed_query);
+// 	for ($card = array (); $row = $result->fetch_assoc(); $card[] = $row){
+// 	}
 	?>
     	
 	<!-- favorites section -->
