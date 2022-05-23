def24c995fe4a54e9f2b0d4508d3aad57df10a19
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 19:46:29 -0400

    Update case2.php

--- uc2-favorites/case2.php
+++ uc2-favorites/case2.php
@@ -46,7 +46,7 @@
 		
 	$constructed_query = "SELECT * FROM STOCK WHERE C_ID < 1000 $category $condition $finish $composition $year";
 	$result = mysqli_query($db, $sql);
-	for ($card1 = array (); $row = $result->fetch_assoc(); $card1[] = $row){
+	for ($card = array (); $row = $result->fetch_assoc(); $card[] = $row){
 	}
 	?>
     	
