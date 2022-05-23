60bf5cf5943b2046ca5338ac243ff8f4a196e84c
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 02:53:02 -0400

    Update add_favorite.php

--- uc2-favorites/add_favorite.php
+++ uc2-favorites/add_favorite.php
@@ -4,8 +4,6 @@
   if(!in_array($_GET['C_ID'], $_SESSION['favorites'])){
     array_push($_SESSION['favorites'], $_GET['C_ID']);
     $_SESSION['message'] = 'Card added to Favorites';
-  }else {
-    $_SESSION['message'] = 'Card already in Favorites' 
   }
 
   header('location: ../uc1-Listings-View/ListingsView.php');
