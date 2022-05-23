e9fd4735f0ab2e377b0357eaaddd37447cc30853
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 00:47:49 -0400

    Update case2.php

--- uc2-favorites/case2.php
+++ uc2-favorites/case2.php
@@ -43,6 +43,24 @@
 		$year = "AND C_YEAR = '$_SESSION[year]'";
 	}
 		
+	$c_ID=$_GET['C_ID'];
+			
+	if(!isset($_SESSION['favorites'])){
+		$favoritesArr = array();
+		$_SESSION['favorites'] = $favoritesArr;
+	}
+
+	array_push($_SESSION['favorites'],$c_ID);
+		
+	if(isset($_SESSION['favorites'])){
+		foreach($_SESSION['favorites'] as $item){
+			$sql = 'SELECT * FROM STOCK WHERE C_ID ='.$item;
+			$result = mysqli_query($db,$sql);
+			$card = mysqli_fetch_array($result);
+		}
+	}else {
+		echo 'There have been no Favorite cards added';		
+	}
 // 	$constructed_query = "SELECT * FROM STOCK WHERE C_ID < 1000 $category $condition $finish $composition $year";
 // 	$result = mysqli_query($db, $constructed_query);
 // 	for ($card = array (); $row = $result->fetch_assoc(); $card[] = $row){
