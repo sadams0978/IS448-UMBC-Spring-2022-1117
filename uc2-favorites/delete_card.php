e4e5aa51568d8a218abde06343b16757f88226ab
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 18:18:50 -0400

    Update delete_card.php

--- uc2-favorites/delete_card.php
+++ uc2-favorites/delete_card.php
@@ -3,11 +3,11 @@
 
 	//remove the id from favaorites array
 	$key = array_search($_GET['C_ID'], $_SESSION['favorites']);	
-	unset($_SESSION['cart'][$key]);
+	unset($_SESSION['favorites'][$key]);
 
 	unset($_SESSION['qty_array'][$_GET['index']]);
 	$_SESSION['qty_array'] = array_values($_SESSION['qty_array']);
 
-	$_SESSION['message'] = "Product deleted from cart";
+	$_SESSION['message'] = "Card Removed From Favorites";
 	header('location: case2.php');
 ?>
