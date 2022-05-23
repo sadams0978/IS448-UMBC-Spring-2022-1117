4eeba19c21eadf289ac359b882fea8db1988f4df
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 13:10:40 -0400

    Update case2.php

--- uc2-favorites/case2.php
+++ uc2-favorites/case2.php
@@ -2,7 +2,7 @@
 	include('../db_connection.php');
 ?>
 <!doctype html>
-<html>
+<html lang ="en">
 	<head>
 		<meta charset="utf-8">
 		<link rel = "stylesheet" type = "text/css" href = "../root.css"/>
