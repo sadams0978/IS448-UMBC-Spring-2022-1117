93641ecfb9f46846957aa505711bc9802f7c0b3b
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 22:34:37 -0400

    Create uc2-favorites-js.js

---
+++ uc2-favorites/uc2-favorites-js.js
@@ -0,0 +1,11 @@
+"use strict";
+window.onload = pageLoad; 
+
+function zoom(e){
+  var zoomer = e.currentTarget;
+  e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+  e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+  x = offsetX/zoomer.offsetWidth*100
+  y = offsetY/zoomer.offsetHeight*100
+  zoomer.style.backgroundPosition = x + '% ' + y + '%';
+}
