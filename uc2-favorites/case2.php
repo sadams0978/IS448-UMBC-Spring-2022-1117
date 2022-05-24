6bb55cc6e6510a8e2c406db7f9dc880e364da712
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 21:28:44 -0400

    Update case2.php

--- uc2-favorites/case2.php
+++ uc2-favorites/case2.php
@@ -51,8 +51,6 @@
  	}
 	$sql = "SELECT * FROM STOCK WHERE C_ID < 1000 $category $condition $finish $composition $year AND C_ID IN (".implode(',',$_SESSION['favorites']).")";
 	$query = $db->query($sql);
-	}else{
-		echo "<h1>No Cards In Favorites</h1>";
 	}
 	?>
     	
