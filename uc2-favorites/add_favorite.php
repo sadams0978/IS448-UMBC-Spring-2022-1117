43f348aea33d3b75a40644cb3bc02bb1a6bb30ce
ddeleon1 <99271492+ddeleon1@users.noreply.github.com>
2022-05-23 21:53:25 -0400

    Update add_favorite.php

--- uc2-favorites/add_favorite.php
+++ uc2-favorites/add_favorite.php
@@ -1,6 +1,5 @@
 <?php
 	include('../db_connection.php');
-	include('../menu.php');
 	//check if card in favorites
 	if(!in_array($_GET['C_ID'], $_SESSION['favorites'])){
 		array_push($_SESSION['favorites'], $_GET['C_ID']);
